ab12c1b32c6b6bdcafb5d765143ed87bb54a42af
Fernanda Vásquez <82715400+cam-vasquez@users.noreply.github.com>
2021-09-24 22:09:19 -0600

    Add files via upload

---
+++ 25-09-2021/ejercicio02/ejercicio02.cpp
@@ -0,0 +1,12 @@
+#include "conteo.hpp"
+
+int main(void){
+    cout << endl;
+    cout << "Programa de Conteo";
+    cout << endl;
+    numeros nuevosDatos;
+    cout << endl;
+    nuevosDatos.menu(0);
+    return 0; 
+}
+
