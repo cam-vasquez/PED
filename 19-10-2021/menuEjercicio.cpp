123541f8850f54903ab4ed1c8049ee14a0ebd391
Fernanda Vásquez <82715400+cam-vasquez@users.noreply.github.com>
2021-10-19 00:57:36 -0600

    Add files via upload

---
+++ 19-10-2021/menuEjercicio.cpp
@@ -0,0 +1,51 @@
+#include "tarea.hpp"
+
+using namespace std; 
+
+int menu(){
+
+    int opcion;
+
+    cout << endl;
+    cout << "\tBienvenido" << endl;
+    cout << "1) Iniciar sesion" << endl;
+    cout << "2) Mostrar usuarios ingresados" << endl;
+    cout << "3) Eliminar usuarios" << endl;
+    cout << "4) Salir." << endl;
+    cout << "Su Opcion: ";
+    cin >> opcion; 
+    cin.ignore();
+    return opcion;
+}
+
+int main(void){
+    LinkedList sesion;
+    
+    bool continuar = true;
+    Informacion p;
+    Informacion pref;
+    do{
+        switch(menu()){
+            case 1: //Agregar una pedido
+                p = solicitarInformacion();
+                sesion.addBack(p);  
+                cout << "Ha ingresado sesion exitosamente" << endl;
+                break;
+            case 2: //Mostrar pedido de inicio a fin
+                sesion.printForward(); 
+                break;
+            case 3: //Eliminar un pedido
+                p = solicitarInformacion(); 
+                sesion.deleteItem(p); 
+                break;
+            case 4: //Salir
+                continuar = false;
+                break;
+            default: //El usuario se equivoco
+                cout << "Ningun usuario ha inciado sesion" << endl;
+                break;
+        }
+    }while(continuar);
+    
+    return 0;
+}
